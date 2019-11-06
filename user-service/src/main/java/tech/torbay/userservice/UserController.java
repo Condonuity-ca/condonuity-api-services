17e791836010bc1d95dc3f0552cae1eeb0fff7fc
m-a-n-a-v <manav.phull@gmail.com>
2019-11-06 13:50:33 -0500

    Added spring Sleuth dependency for ID tracing

--- user-service/src/main/java/tech/torbay/userservice/UserController.java
+++ user-service/src/main/java/tech/torbay/userservice/UserController.java
@@ -1,5 +1,7 @@
 package tech.torbay.userservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,20 @@ import java.util.List;
 @RequestMapping("/")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("/users")
     public List<String> getUsers() {
+
+        logger.info("Processing Users Request");
+
         List<String> users = Arrays.asList(
                 new String("One"),
                 new String("Two")
         );
+
+        logger.info("Processed User Request");
+
         return users;
     }
 
