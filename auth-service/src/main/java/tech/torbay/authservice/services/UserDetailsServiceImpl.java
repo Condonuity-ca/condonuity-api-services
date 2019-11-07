7fd9c4bacfb9193ef185f262a7a7995fcdf2d451
m-a-n-a-v <manav.phull@gmail.com>
2019-11-06 21:42:14 -0500

    Added the users for access to portal

--- auth-service/src/main/java/tech/torbay/authservice/services/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/tech/torbay/authservice/services/UserDetailsServiceImpl.java
@@ -26,8 +26,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         // hard coding the users. All passwords must be encoded.
         final List<AppUser> users = Arrays.asList(
-                new AppUser(1, "omar", encoder.encode("12345"), "USER"),
-                new AppUser(2, "admin", encoder.encode("12345"), "ADMIN")
+                new AppUser(1, "manav", encoder.encode("12345"), "USER"),
+                new AppUser(2, "admin", encoder.encode("12345"), "ADMIN"),
+                new AppUser(3, "nagaraj", encoder.encode("12345"), "ADMIN"),
+                new AppUser(4, "manoj", encoder.encode("12345"), "ADMIN")
         );
 
 
